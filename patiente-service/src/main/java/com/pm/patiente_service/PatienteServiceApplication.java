0f453b0d46ebe2ab27642e633c815fd6b87da87a
Arllain <arllain@gmail.com>
2025-03-31 22:08:22 -0300

    patiente-service module creation

---
+++ patiente-service/src/main/java/com/pm/patiente_service/PatienteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.patiente_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatienteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatienteServiceApplication.class, args);
+	}
+
+}
---
+++ patiente-service/src/test/java/com/pm/patiente_service/PatienteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.patiente_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatienteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
